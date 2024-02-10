f83fdec6871ba4e474ec598b12ed0594b73cc6c9
Miguel Vargas <miguel.vargas.dev@outlook.com>
2024-02-10 17:29:57 -0500

    Top Level Statements.

---
+++ 3TopLevelStatementsExample/TopLevelStatementsExample/Controllers/WeatherController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using TopLevelStatementsExample.Models;
+
+namespace TopLevelStatementsExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private static readonly string[] Summaries =
+        [
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        ];
+        
+        // GET: api/<weather>
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                    })
+                .ToArray();
+            return forecast;
+        }
+        
+        // GET api/<weather>/5
+        [HttpGet("{id}")]
+        public WeatherForecast Get(int id)
+        {
+            return new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(id)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            };
+        }
+        
+        // POST api/<weather>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<weather>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<weather>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ 3TopLevelStatementsExample/TopLevelStatementsExample/Program.cs
@@ -0,0 +1,15 @@
+using TopLevelStatementsExample;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var startup = new Startup(builder.Configuration);
+        
+// Add services to the container.
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+        
+// Use services added above
+startup.Configure(app, app.Environment);
+
+app.Run();
