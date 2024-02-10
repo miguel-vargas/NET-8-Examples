a9ad8fb398d2c1bd4b71f80cbf43a8b0de5f4c75
Miguel Vargas <miguel.vargas.dev@outlook.com>
2024-02-10 17:28:53 -0500

    Startup example

---
+++ 2StartupExample/StartupExample/Startup.cs
@@ -0,0 +1,41 @@
+namespace StartupExample;
+
+public class Startup
+{
+    private IConfiguration Configuration { get; }
+    
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+    
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Add services to the container.
+        services.AddAuthorization();
+        
+        // NOTES: Instead of FastAPI we are using traditional controllers.
+        services.AddControllers();
+    
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(WebApplication app, IHostEnvironment env)
+    {
+        // Configure the HTTP request pipeline.
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        // NOTES: Need this to use the controllers added above.
+        app.MapControllers();
+    }
+}
